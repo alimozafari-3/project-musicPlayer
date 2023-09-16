4069889a67fd0c084c366c7c3cd85078b0655652
ali <ali.mozafari733@yahoo.com>
2023-09-16 11:27:40 +0430

    add js select elman

--- music.js
+++ music.js
@@ -1,1 +1,14 @@
 
+let play = document.getElementById("playpus");
+let audio = document.getElementById("audio");
+let rangtme = document.querySelector(".zaman");
+let dt2 = document.querySelector(".date2");
+let dt1 = document.querySelector(".date1");
+let nameplay1 = document.getElementById("nameplay");
+let nameplay2 = document.getElementById("namemuzic");
+let sefr = 0;
+let date1 = 0;
+let date2 = 0;
+let setinter;
+let setinterval;
+let index = 0;
